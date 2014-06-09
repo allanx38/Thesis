9b3730ab81de908a0756cf727cfc1076e917f700
allanx38 <allanx38@yahoo.co.uk>
2014-06-09 16:49:01 +0100

    Update Candle_Hammer.R

--- RCode/Candle_Hammer.R
+++ RCode/Candle_Hammer.R
@@ -12,8 +12,6 @@ candle_hammer <- function(Mkt, SLoss, MktName){
 
   Mkt$prev_Hammer   <- c( NA, Mkt$Hammer[ - length(Mkt$Hammer) ] )
   Mkt$prev_Inv_Hammer   <- c( NA, Mkt$InvertedHammer[ - length(Mkt$InvertedHammer) ] )
-  #Mkt$prev_Hanging_Man   <- c( NA, Mkt$Hammer[ - length(Mkt$Hammer) ] )
-  #Mkt$prev_Shooting_Star   <- c( NA, Mkt$InvertedHammer[ - length(Mkt$InvertedHammer) ] )
   
   # Trade Long
   Mkt$Long <- ifelse(Mkt$prev_Hammer==TRUE | Mkt$prev_Inv_Hammer==TRUE, Mkt$Close-Mkt$Open, NA)
@@ -25,17 +23,6 @@ candle_hammer <- function(Mkt, SLoss, MktName){
                        Mkt$Long)
     results["LongPL"] <- round(sum(Mkt$Long, na.rm=TRUE))
   }
-  
-  # Trade Short
-  # Mkt$Short <- ifelse(Mkt$prev_Aroon_UP >= 70, ifelse(Mkt$prev_Hammer=='Shooting Star' | Mkt$Hammer=='Hanging Man', Mkt$Close-Mkt$Open, NA) ,NA)
-  # results["ShortPL"] <- round(sum(Mkt$Short, na.rm=TRUE))
-  #Adj for SLoss
-  #   if (SLoss < 0){
-  #     Mkt$Short <- ifelse((Mkt$Open - Mkt$Low) > 0,
-  #                         ifelse((Mkt$Open-Mkt$High) < SLoss, SLoss, Mkt$Short),
-  #                         Mkt$Short)
-  #     results["ShortPL"] <- round(sum(Mkt$Short, na.rm=TRUE))
-  #   }
   
   Stats <- calcStats(Mkt$Long)
   results[5:7] <- Stats
