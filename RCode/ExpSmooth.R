cf778bd51d38cc07d90a06d7afb39fffbc303225
allanx38 <allanx38@yahoo.co.uk>
2014-07-02 07:43:38 +0100

    0207

--- RCode/ExpSmooth.R
+++ RCode/ExpSmooth.R
@@ -1,13 +1,39 @@
 # 1. Exp Smoothing
+setwd("D:/Allan/DropBox/MSc/Dissertation/Thesis/RCode")
+library(forecast)
+
+exp_sm <- function(Mkt_ts, Mkt, strt){
+  #browser()
+  Mkta <- Mkt
+  cc <- Mkta[1,]
+  cc$a <- 0
+  ln <- nrow(Mkt)
+  #lb <- 300 #lookback
+  for(i in strt:ln){
+    st <- i-30
+    Mkt_slice <- window(Mkt_ts,start=st,end=i)
+    modf <- ets(Mkt_slice)
+    #fcast <- forecast.ets(mod)
+    #a <- fcast$fitted[300]
+    a <- modf$method
+    b <- Mkta[i,]
+    ab <- cbind(b,a)
+    cc <- rbind(cc,ab)
+  }
+  cc <- cc[-1,]
+  return(cc)
+}
 
 Mkt <- read.csv("../Data/Dax_2000_d.csv")
 tail(Mkt)
 Mkt_ts <- ts(Mkt$Close)
+Mkt_train <- window(Mkt_ts, start=2000, end=2030)
 mod <- ets(Mkt_ts)
-mod
+mod$method
 mod$fitted
 fcast <- forecast.ets(mod, h=5)
-fcast
+
+as <- exp_sm(Mkt_ts,Mkt, 3000)
 
 nrow(Mkt)
 Mkt_train <- window(Mkt_ts, start=2000, end=2030)
@@ -52,8 +78,35 @@ fcast2
 #   cc <- cc[-1,]
 #   return(cc)
 # }
-# 
-# Mkt <- read.csv("../Data/Dax_2000_d.csv")
-# Mkt_ts <- ts(Mkt$Close)
-# res <- exp_sm(Mkt_ts, Mkt, 3500)
-# write.csv(res,'../Data/Dax_ets_aan_300.csv')
+
+#aroon
+Mkt_ar <- read.csv("../Data/Dax_2000_d.csv")
+ar <- aroon(Mkt_ar[c(3,4)], n=20)
+ar <- aroon(Mkt_ar[c(5)], n=20)
+tail(ar,n=20)
+
+Mkt_ar2 <- cbind(Mkt_ar,ar)
+tail(Mkt_ar2,n=20)
+
+nrow(Mkt_ar2)
+aup <- Mkt_ar2[Mkt_ar2$aroonUp > 0.65 & Mkt_ar2$aroonDn < 0.4,]
+nrow(aup)
+adn <- Mkt_ar2[Mkt_ar2$aroonDn > 0.65 & Mkt_ar2$aroonUp < 0.35,]
+nrow(adn)
+
+
+
+run_aroon_sys <- function(fil,SLoss,nm){
+  df10 <- as.data.frame(matrix(seq(11),nrow=1,ncol=11))
+  for(i in 1:length(fil)){
+    Mkt <- read.csv(fil[i])
+    ar <- aroon(Mkt[c(3,4)], n=20)                 #calc Aroon values
+    Mkt <- cbind(Mkt, ar)                           #Add Aroon values to orig data set
+    a <- aroon_sys(Mkt, SLoss, nm[i])
+    df10 <- rbind(df10,a)
+  }
+  df.name <- names(a)
+  names(df10) <- df.name
+  df10 <- df10[-1,]
+  return(df10)
+}
